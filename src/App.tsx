4f8dc24132a613165980a98f0d6d2d610d1f6c7a
IAN LUMBERA <ianlumbera@IANs-MacBook-Air.local>
2026-08-21 17:48:20 -0700

    Add TanStack Query DevTools badge for visual debugging

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import Layout from './components/Layout';
 import DashboardPage from './pages/DashboardPage';
 import ItemsPage from './pages/ItemsPage';
@@ -9,18 +10,21 @@ import NotFoundPage from './pages/NotFoundPage';
 import ProtectedRoute from './components/ProtectedRoute';
 
 const App: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<Layout />}>
-      <Route index element={<DashboardPage />} />
-      <Route path="items" element={<ItemsPage />} />
-      <Route path="items/:id" element={<ItemDetail />} />
-      <Route path="login" element={<LoginPage />} />
-      <Route path="protected" element={<ProtectedRoute />}>
-        <Route index element={<div className="p-6">Protected content</div>} />
+  <>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<DashboardPage />} />
+        <Route path="items" element={<ItemsPage />} />
+        <Route path="items/:id" element={<ItemDetail />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="protected" element={<ProtectedRoute />}>
+          <Route index element={<div className="p-6">Protected content</div>} />
+        </Route>
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
-      <Route path="*" element={<NotFoundPage />} />
-    </Route>
-  </Routes>
+    </Routes>
+    <ReactQueryDevtools initialIsOpen={false} />
+  </>
 );
 
 export default App;
